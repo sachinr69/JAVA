8be938a20453946a90482cb5661ff21658054fbc
sachin kumar <sn08064021@gmail.com>
2024-08-26 12:12:46 +0530

    Gender Interest

---
+++ Assignment1/Q6.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Q6 {
+    public static void main(String[] args){
+        String gender=args[0];
+		int age=Integer.parseInt(args[1]);
+
+		if(gender.equalsIgnoreCase("Female")) {
+
+		   if(age>=1&&age<=58)
+		        System.out.println("Interest=8.2%");
+
+			else if(age>58&&age<=100)
+			    System.out.println("Interest=9.2%");
+
+			else
+				System.out.println("No result");
+
+		}
+		
+		else {
+
+		       if(age>=1&&age<=58)
+			      System.out.println("Interest=8.4%");
+
+		       else if(age>58&&age<=100)
+			       System.out.println("Interest=10.5%");
+
+			   else 
+				   System.out.println("No result");
+
+		}
+
+    }
+    
+}
